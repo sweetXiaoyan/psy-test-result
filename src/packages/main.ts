6d00e5e46deaddfb06160168dd9bc166fa4c64dd
xiaoxiao <1027535391@qq.com>
2021-07-08 14:10:35 +0800

    all

--- src/packages/main.ts
+++ src/packages/main.ts
@@ -1,5 +1,5 @@
 // export { default as JsonChart} from "./"
-export { default as YCharts} from "./main/index"
-export { default as JsonPie} from "./Pie"
-export { default as JsonRadar} from "./Radar"
-export { default as JsonBar} from "./Bar"
+export { default as JCharts } from "./main/index";
+export { default as JsonPie } from "./Pie";
+export { default as JsonRadar } from "./Radar";
+export { default as JsonBar } from "./Bar";
